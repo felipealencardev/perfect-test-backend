2a6fbf0e6d01073ffeb81186f237d203aa65fa0b
Felipe Alencar <felipealencardev@gmail.com>
2021-03-01 23:06:45 -0300

    Feature: cria models e migrations de client, product e sales

---
+++ database/migrations/2021_03_02_015351_create_sales_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('product_id');
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->unsignedBigInteger('client_id');
+            $table->foreign('client_id')->references('id')->on('clients');
+            $table->integer('quantity')->nullable();
+            $table->double('discount', 8, 2)->nullable();
+            $table->enum('status', ['APPROVED', 'CANCELED', 'RETURNED'])->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales');
+    }
+}
