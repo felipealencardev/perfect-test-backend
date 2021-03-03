a3d5498a812474ff6036d9d2357d9e6e973104a0
Felipe Alencar <felipealencardev@gmail.com>
2021-03-03 20:19:32 -0300

    Fixed: add campo date com mutator

--- app/Models/Sale.php
+++ app/Models/Sale.php
@@ -12,8 +12,18 @@ class Sale extends Model
     protected $fillable = [
         'product_id',
         'client_id',
+        'date',
         'quantity',
         'discount',
         'status'
     ];
+
+    public function setDateAttribute($date) {
+        $this->attributes['date'] = Carbon::createFromDate($date)->format('Y-m-d');
+    }
+
+    public function getDateAttribute($date) {
+        return Carbon::createFromFormat('dd/MM/yyyy', $date);
+    }
+
 }
--- database/migrations/2021_03_02_015351_create_sales_table.php
+++ database/migrations/2021_03_02_015351_create_sales_table.php
@@ -19,6 +19,7 @@ class CreateSalesTable extends Migration
             $table->foreign('product_id')->references('id')->on('products');
             $table->unsignedBigInteger('client_id');
             $table->foreign('client_id')->references('id')->on('clients');
+            $table->date('date');
             $table->integer('quantity')->nullable();
             $table->double('discount', 8, 2)->nullable();
             $table->enum('status', ['APPROVED', 'CANCELED', 'RETURNED'])->nullable();
